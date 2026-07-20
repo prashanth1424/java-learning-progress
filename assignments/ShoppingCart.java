8ab5796f4f5fecb7961401390cc4e20236110343
prashanth1424 <gattuprashanth85@gmail.com>
2026-07-20 09:45:43 +0530

    Describe your updates here--2

---
+++ assignments/ShoppingCart.java
@@ -0,0 +1,16 @@
+// Ecommerce application
+public class ShoppingCart{
+    private Product[] items = new Product[10];
+
+    private List<Product> items = new ArrayList<>();
+
+    public void addItem(Product product) {
+        items.add(item);
+
+
+
+    students.add("Alice")
+
+    }
+}
+
